4660e6b48437f5bd4e2f49eb86b19a57c8326241
ThanushSupra <s.thanushkanth11@gmail.com>
2026-03-06 17:37:33 -0500

    Updated experience

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -8,15 +8,25 @@ export interface Experience {
 }
 
 export const experiences: Experience[] = [
+  { 
+    company: 'Rocket Companies',
+    position: 'Software Develoepr Co-op', 
+    techstack: 'Angular, Typescript, C#, MySQL', 
+    location: 'Remote',
+    duration: 'May 2026 - August 2026', 
+    description: [ 
+      'Incoming S26 Intern'
+    ]
+  },
   {
     company: 'Weve',
     position: 'Software Engineering Intern',
-    techstack: 'React, TypeScript, Tailwind, Supabase, Mapbox GL, Apple Maps',
+    techstack: 'React, Typescript, Tailwind, Supabase, Mapbox GL, Apple Maps',
     location: 'Toronto, Canada',
     duration: 'May 2025 – Aug 2025',
     description: [
       'Helped design, build, and launch a community events app with secure authentication, personalized onboarding, and interactive map-based event discovery.',
       'Developed a similarity-based matching system, improved search performance, and led development of the companion web platform.',
     ],
-  },
+  }
 ];
