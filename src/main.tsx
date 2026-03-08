52cc5e21fad68494aa9a52e01a6e8a734e629704
ThanushSupra <s.thanushkanth11@gmail.com>
2026-03-08 12:37:23 -0400

    added analytics using vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Portfolio from './Portfolio'
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
     <Portfolio />
   </StrictMode>,
 )
